e891277f07fc82e0b0482bb67dfdcb5876812fde
Jeanniton Monero <dev.mnr.j@gmail.com>
2018-04-08 16:55:43 +0000

    Adding hello & mario

---
+++ pset6/mario/mario.py
@@ -0,0 +1,29 @@
+
+
+# Print mario wall
+
+
+
+def main():
+
+    # The height of the pyramid
+    height = -1
+
+    # Get pyramid height. re-ask if given value isn't valid range
+    while height < 0 or height > 23:
+        print("Pyramid height: ")
+        height = (int)(input())
+
+    # Build the pyramid
+    for i in range(height):
+        # Print the spaces within this row
+        for j in range(height-i-1):
+            print(" ", end="")
+        for k in range(i+2):
+            print("#", end="")
+        # Go to new row
+        print("")
+
+
+if __name__ == "__main__":
+    main()
